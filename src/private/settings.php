4692c0ef8b86200020becc7e58147d063df6ad5b
Michael Peacock <mkpeacock@gmail.com>
2014-12-27 13:08:37 +0000

    Pusher settings

--- src/private/settings.php
+++ src/private/settings.php
@@ -3,4 +3,7 @@
 $settings = [
     'sid' => 'twilio-sid-goes-here',
     'token' => 'twilio-token-goes-here',
+    'key' => 'pusher-key-goes-here',
+    'secret' => 'pusher-secret-goes-here',
+    'app_id' => 'pusher-app-id-goes-here',
 ];
